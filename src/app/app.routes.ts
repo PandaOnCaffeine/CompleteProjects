77f9c37adcd96eb45a27f4df4475ff5e6f6598de
Pandaa <TuePVM@gmail.com>
2024-02-13 12:38:52 +0100

    Startup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//Geometri
+import { HomeComponent } from './components/geometri/home/home.component';
+import { SquareComponent } from './components/geometri/square/square.component';
+import { RectangleComponent } from './components/geometri/rectangle/rectangle.component';
+
+export const routes: Routes = [
+    { path: 'geometri', component: HomeComponent, pathMatch: 'full' },
+    { path: 'square', component: SquareComponent, pathMatch: 'full' },
+    { path: 'rectangle', component: RectangleComponent, pathMatch: 'full' },
+
+];
