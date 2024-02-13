734f4ce4ffb445b3eb8524049a5233138fd304e0
Pandaa <TuePVM@gmail.com>
2024-02-13 13:24:15 +0100

    Geometri change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 
-//Geometri
-import { HomeComponent } from './components/geometri/home/home.component';
+// Geometri
+import { GeometriHomeComponent } from './components/geometri/home/home.component';
 import { SquareComponent } from './components/geometri/square/square.component';
 import { RectangleComponent } from './components/geometri/rectangle/rectangle.component';
+import { ElectricHomeComponent } from './components/electricCars/home/home.component';
+
+// Electric Cars
 
 export const routes: Routes = [
-    { path: 'geometri', component: HomeComponent, pathMatch: 'full' },
+    { path: 'geometri', component: GeometriHomeComponent, pathMatch: 'full' },
     { path: 'square', component: SquareComponent, pathMatch: 'full' },
     { path: 'rectangle', component: RectangleComponent, pathMatch: 'full' },
-
+    { path: 'cars', component: ElectricHomeComponent, pathMatch: 'full' },
 ];
--- src/app/components/geometri/home/home.component.ts
+++ src/app/components/geometri/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 
-import { MatCardModule } from '@angular/material/card'
-import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { SquareComponent } from '../square/square.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatTabsModule, SquareComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class GeometriHomeComponent {
 
 }
